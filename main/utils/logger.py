9a72eaa6777911a86609eaa1b8862520380eae3e
Hung Vu <vudinhhung942k@gmail.com>
2021-06-17 02:43:12 +0700

    fix: Proper logger name

--- main/utils/logger.py
+++ main/utils/logger.py
@@ -3,5 +3,5 @@ from logging.handlers import RotatingFileHandler
 
 file_handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=4)
 file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
-logger = logging.getLogger('request_logger')
+logger = logging.getLogger(__name__)
 logger.addHandler(file_handler)
