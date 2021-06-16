078c279a3811cba0a62db124d5162655aac72e73
Hung Vu <vudinhhung942k@gmail.com>
2021-06-16 17:41:52 +0700

    refactor: remove old 'build' and 'client' folders

--- client/src/components/MessageBanner.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Alert } from 'react-bootstrap';
-
-const MessageBanner = ({ message }) => {
-  return <Alert variant={message.startsWith('Error') ? 'danger' : 'info'}>{message}</Alert>;
-};
-
-export default MessageBanner;
