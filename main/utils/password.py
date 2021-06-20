3bc11df809de599dc436037389a65cc25ead4d61
Hung Vu <vudinhhung942k@gmail.com>
2021-06-20 07:30:16 +0700

    refactor: Use a constant variable for salt rounds for clarity

--- main/utils/password.py
+++ main/utils/password.py
@@ -1,10 +1,14 @@
 """Utils for hashing and comparing passwords"""
 import bcrypt
 
+# This is the cost factor needed to calculate a hash. It is set to the minimum factor I benchmarked
+# to be needed for bcrypt to take around 1s to hash a password on a 2015 MacBook Pro
+SALT_ROUNDS = 14
+
 
 def hash_password(password):
     """Return a secure hash for the given password"""
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(14))
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt(SALT_ROUNDS))
 
 
 def check_password(password, password_hash):
